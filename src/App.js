0fbe09be9037897bd771ae6be35e2df29676f8aa
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-02-02 00:42:58 -0300

    Initial version with request, name and front image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class PokemonImage extends React.Component {
+  render() {
+    if (this.props.pokemon != null) {
+      return (
+        <img
+          src={this.props.pokemon.sprites.front_default}
+          alt="Pokémon sprite"
+        />
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.test = this.test.bind(this);
+  }
+
+  test(e) {
+    e.preventDefault();
+    let pokemonName = e.target.pokemonName.value;
+    console.log(pokemonName);
+    this.props.onChange(pokemonName);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.test}>
+        <label>
+          Pokémon name:
+          <input type="text" name="pokemonName" />
+        </label>
+        <input type="submit" value="Search" />
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemonName: "",
+      searchResult: "",
+      pokemon: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.searchPokemon = this.searchPokemon.bind(this);
+  }
+
+  handleChange(pokemonName) {
+    this.setState({ pokemonName: pokemonName });
+    this.searchPokemon(pokemonName);
+  }
+
+  searchPokemon(pokemonName) {
+    const Pokedex = require("pokeapi-js-wrapper");
+    const P = new Pokedex.Pokedex();
+    console.log(pokemonName);
+    let self = this;
+
+    P.getPokemonByName(pokemonName).then(function(response) {
+      console.log(response);
+      self.setState({ pokemon: response });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchForm onChange={this.handleChange} />
+        <br />
+        {this.state.pokemonName}
+        <br />
+        <PokemonImage pokemon={this.state.pokemon} />
+      </div>
+    );
+  }
 }
 
 export default App;
