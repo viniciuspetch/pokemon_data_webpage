eb21c281143c7d4e394330ffaaec3853e892aef8
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-02-20 15:26:28 -0300

    Modularized and added some tabs

--- src/App.js
+++ src/App.js
@@ -62,6 +62,81 @@ class PokemonName extends React.Component {
   }
 }
 
+class PokemonDataTab extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.getAbilities = this.getAbilities.bind(this);
+    this.getTypes = this.getTypes.bind(this);
+  }
+
+  getAbilities() {
+    let abilitiesList = "";
+    if (this.props.pokemon != null) {
+      abilitiesList = this.props.pokemon.abilities.map(ability => {
+        let url_prefix = "https://bulbapedia.bulbagarden.net/wiki/";
+        let url_suffix = "_(Ability)";
+        let name_fixed = ability.ability.name
+          .split("-")
+          .map(part => part[0].toUpperCase() + part.slice(1))
+          .join(" ");
+
+        return (
+          <a href={url_prefix + name_fixed.replace(" ", "_") + url_suffix}>
+            <div className="listItem">{name_fixed}</div>
+          </a>
+        );
+      });
+    }
+
+    return abilitiesList;
+  }
+
+  getTypes() {
+    let typesList = "";
+    if (this.props.pokemon != null) {
+      typesList = this.props.pokemon.types.map(type => {
+        let url_prefix = "https://bulbapedia.bulbagarden.net/wiki/";
+        let url_suffix = "_(type)";
+        let name_fixed =
+          type.type.name[0].toUpperCase() + type.type.name.slice(1);
+
+        return (
+          <a href={url_prefix + name_fixed + url_suffix}>
+            <div className="listItem">{name_fixed}</div>
+          </a>
+        );
+      });
+    }
+
+    return typesList;
+  }
+
+  render() {
+    let abilitiesList = this.getAbilities();
+    let typesList = this.getTypes();
+
+    return (
+      <div className="lists">
+        <div className="list abilitiesList">{abilitiesList}</div>
+        <div className="list typesList">{typesList}</div>
+      </div>
+    );
+  }
+}
+
+class PokemonStatsTab extends React.Component {
+  render() {
+    return <p>Stats Tab</p>;
+  }
+}
+
+class PokemonMovesetTab extends React.Component {
+  render() {
+    return <p>Moveset Tab</p>;
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -69,10 +144,12 @@ class App extends React.Component {
     this.state = {
       pokemonName: "",
       searchResult: "",
-      pokemon: null
+      pokemon: null,
+      currTab: 1
     };
     this.handleChange = this.handleChange.bind(this);
     this.searchPokemon = this.searchPokemon.bind(this);
+    this.setTab = this.setTab.bind(this);
   }
 
   handleChange(pokemonName) {
@@ -93,6 +170,19 @@ class App extends React.Component {
       .catch(function(error) {
         self.setState({ pokemon: null });
       });
+  }
+
+  setTab(e) {
+    /*
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.id);    
+    console.log(e.target.value);
+    console.log(parseInt(e.target.value));
+    */
+    this.setState({
+      currTab: parseInt(e.target.value)
+    });
   }
 
   render() {
@@ -128,17 +218,38 @@ class App extends React.Component {
       });
     }
 
+    let currTab = null;
+    switch (this.state.currTab) {
+      case 1:
+        currTab = <PokemonDataTab pokemon={this.state.pokemon} />;
+        break;
+      case 2:
+        currTab = <PokemonStatsTab />;
+        break;
+      case 3:
+        currTab = <PokemonMovesetTab />;
+        break;
+      default:
+        currTab = <h1>Error</h1>;
+    }
+
     return (
       <div className="App">
         <SearchForm onChange={this.handleChange} />
         <PokemonName pokemon={this.state.pokemon} />
+        <button value="1" onClick={this.setTab}>
+          Data
+        </button>
+        <button value="2" onClick={this.setTab}>
+          Stats
+        </button>
+        <button value="3" onClick={this.setTab}>
+          Moveset
+        </button>
         <div>
           <PokemonImage pokemon={this.state.pokemon} />
         </div>
-        <div className="lists">
-          <div className="list abilitiesList">{abilitiesList}</div>
-          <div className="list typesList">{typesList}</div>
-        </div>
+        <div>{currTab}</div>
       </div>
     );
   }
